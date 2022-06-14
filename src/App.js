a0dc4ebdf26c12a16758c1d83eb546000ff7635c
mi-hee-k <kim323mh@gmail.com>
2022-06-14 18:35:44 +0900

    data rendering, add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DairyEditor";
+import DiaryList from "./DiaryList";
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "뽀로로",
+//     content: "크롱좋아",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "뽀로로",
+//     content: "포비좋아",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "뽀로로",
+//     content: "해리싫어",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
